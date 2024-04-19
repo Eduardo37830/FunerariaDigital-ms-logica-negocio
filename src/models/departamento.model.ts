6ef5abc3d5361199796b2356cc7cfc134858f927
Jorge Ivan Garcia Torres <111246512+JorgeGarcia105@users.noreply.github.com>
2024-04-19 10:07:05 -0500

    Creación de índices de llaves Foráneas (Foreing Key) ms-lógica-negocio

--- src/models/departamento.model.ts
+++ src/models/departamento.model.ts
@@ -1,4 +1,4 @@
-import {Entity, model, property, hasMany} from '@loopback/repository';
+import {Entity, hasMany, model, property} from '@loopback/repository';
 import {Ciudad} from './ciudad.model';
 
 @model()
@@ -14,7 +14,7 @@ export class Departamento extends Entity {
     type: 'string',
     required: true,
   })
-  Nombre: string;
+  nombre: string;
 
   @hasMany(() => Ciudad)
   ciudads: Ciudad[];
