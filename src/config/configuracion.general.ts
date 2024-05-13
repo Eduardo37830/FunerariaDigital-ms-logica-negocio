d0f60ff1e6d753a2db8b5728e20670fd1e5a9175
Jorge Ivan Garcia Torres <111246512+JorgeGarcia105@users.noreply.github.com>
2024-05-12 19:25:38 -0500

    Manejo de archivo file

---
+++ src/config/configuracion.general.ts
@@ -0,0 +1,7 @@
+export namespace ConfiguracionGeneral {
+  export const carpetaArchivosClientes = "../../archivos/clientes";
+  export const campoDeCliente: string = "file";
+  export const extensionesImagenes: string[] = [".PNG", ".JPG", ".JPEG", ".GIF", ".SVG", ".WEBP"];
+}
+
+
