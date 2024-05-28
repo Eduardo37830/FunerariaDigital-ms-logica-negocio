95fd05110564de316ca39e01d0f7763734a69ddb
Jorge Ivan Garcia Torres <111246512+JorgeGarcia105@users.noreply.github.com>
2024-05-28 16:34:00 -0500

    Continuacion de pasarela de pago y comienzo de depuracion del código

--- src/controllers/cliente-plan.controller.ts
+++ src/controllers/cliente-plan.controller.ts
@@ -102,48 +102,6 @@ export class ClientePlanController {
         );
       }
 
-      // Configuración de Epayco
-      const epayco = require('epayco-sdk-node')({
-        apiKey: ConfiguracionPagos.apiKey,
-        privateKey: ConfiguracionPagos.privateKey,
-        lang: 'ES',
-        test: true
-      })
-
-      //Datos de la tarjeta de crédito de Prueba de Epaycpo
-      //  --- No cambiar
-      const credit_info = {
-        "card[number]": "4575623182290326",
-        "card[exp_year]": "2025",
-        "card[exp_month]": "12",
-        "card[cvc]": "123",
-        "hasCvv": true //hasCvv: validar codigo de seguridad en la transacción
-      }
-
-      epayco.token.create(credit_info)
-        .then(function (token: any) {
-          console.log(token);
-          console.log("Transacción exitosa" + token);
-        })
-        .catch(function (err: string) {
-          console.log("err: " + err);
-        });
-
-      const customer_info = {
-        token_card: "toke_id",
-        name: cliente.primerNombre,
-        last_name: cliente.primerApellido,
-        email: cliente.correo,
-        default: true,
-        //Optional parameters: These parameters are important when validating the credit card transaction
-        city: cliente.ciudadResidencia,
-        address: cliente.direccion,
-        phone: cliente.celular,
-        cell_phone: "3010000001"
-      }
-
-      console.log('customer_info', customer_info);
-
       // Crea la nueva asociación ClientePlan
       console.log('PLAN AQUIRIDO!!!');
 
@@ -258,141 +216,15 @@ export class ClientePlanController {
     await this.clientePlanRepository.deleteById(id); // Eliminamos el ClientePlan por su ID
   }
 
-  @put('/modificar-plan/{id}', {
-    responses: {
-      '200': {
-        description: 'Cliente.Plan PUT success count',
-        content: {'application/json': {schema: CountSchema}},
-      },
-    },
+  @put('/modificar-plan/{id}')
+  @response(204, {
+    description: 'Cliente.Plan PUT success',
   })
-  async modificarPlan(
+  async replaceById(
     @param.path.number('id') id: number,
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(ClientePlan, {partial: true}),
-        },
-      },
-    })
-    clientePlanData: Partial<ClientePlan>,
+    @requestBody() clientePlan: ClientePlan,
   ): Promise<void> {
-    const clientePlanExists = await this.clientePlanRepository.exists(id); // Verificamos si el ClientePlan existe
-
-    if (!clientePlanExists) {
-      throw new HttpErrors.NotFound(`No se encontró ninguna asociación ClientePlan con la ID ${id}`);
-    }
-
-    const cliente = await this.clienteRepository.findById(clientePlanData.clienteId);
-
-    if (!cliente) {
-      throw new HttpErrors.NotFound(`No se encontró el cliente con la ID ${clientePlanData.clienteId}`);
-    }
-
-    // Configuración de Epayco
-
-    const epayco = require('epayco-sdk-node')({
-      apiKey: ConfiguracionPagos.apiKey,
-      privateKey: ConfiguracionPagos.privateKey,
-      lang: 'ES',
-      test: true
-    })
-
-
-    const credit_info = {
-      "card[number]": "4575623182290326",
-      "card[exp_year]": "2025",
-      "card[exp_month]": "12",
-      "card[cvc]": "123",
-      "hasCvv": true //hasCvv: validar codigo de seguridad en la transacción
-    }
-
-    const token = await epayco.token.create(credit_info);
-    console.log("Token creado:", token);
-
-    if (!token.id) {
-      throw new HttpErrors.InternalServerError("Error al crear el token de la tarjeta");
-    }
-
-    const customer_info = {
-      token_card: token.id,
-      name: cliente.primerNombre,
-      last_name: cliente.primerApellido,
-      email: cliente.correo,
-      default: true,
-      city: cliente.ciudadResidencia,
-      address: cliente.direccion,
-      phone: cliente.celular,
-      cell_phone: "3010000001"
-    };
-
-    const customer = await epayco.customers.create(customer_info);
-    console.log("Cliente creado:", customer);
-
-    if (!customer.data || !customer.data.customerId) {
-      throw new HttpErrors.InternalServerError("Error al crear el cliente");
-    }
-
-    const plan_info = {
-      id_plan: clientePlanData.id?.toString(),
-      name: clientePlanData.nombre,
-      description: clientePlanData.detalles,
-      amount: clientePlanData.tarifa,
-      currency: "cop",
-      interval: "month",
-      interval_count: 1,
-      trial_days: 30
-    };
-
-    if (!plan_info.id_plan) {
-      const plan = await epayco.plans.create(plan_info);
-      console.log("Plan creado:", plan);
-      if (!plan.data || !plan.data.planId) {
-        throw new HttpErrors.InternalServerError("Error al crear el plan");
-      }
-    }
-
-
-    const pse_info = {
-      bank: "1022",
-      invoice: clientePlanData.id?.toString(),
-      description: "pay test",
-      value: clientePlanData.tarifa!.toString(),
-      tax: "0",
-      tax_base: "0",
-      currency: "COP",
-      type_person: "0",
-      doc_type: "CC",
-      doc_number: "123456",
-      name: "testing",
-      last_name: "PAYCO",
-      email: "no-responder@payco.co",
-      country: "CO",
-      cell_phone: "3010000001",
-      ip: "190.000.000.000", /*This is the client's IP, it is required */
-      url_response: "https://ejemplo.com/respuesta.html",
-      url_confirmation: "https://ejemplo.com/confirmacion",
-      metodoconfirmacion: "GET",
-    };
-
-    console.log("Información de la transacción:", pse_info);
-
-    const bankTransaction = await epayco.bank.create(pse_info);
-
-    console.log("Transacción bancaria:", bankTransaction);
-
-    console.log("Transacción bancaria:", bankTransaction);
-
-    if (!bankTransaction || !bankTransaction.success) {
-      console.error("Error en la transacción bancaria:", bankTransaction);
-      throw new HttpErrors.InternalServerError("Error en la transacción bancaria");
-    }
-
-    await this.clientePlanRepository.updateById(id, clientePlanData);
-    console.log('Plan modificado exitosamente');
-  } catch(err: any) {
-    console.log("Error en la transacción", err);
-    throw new HttpErrors.InternalServerError("Error en la transacción con Epayco");
+    await this.clientePlanRepository.replaceById(id, clientePlan);
   }
 
   @post('/pago-epayco', {
@@ -491,7 +323,7 @@ export class ClientePlanController {
     }
 
 
-    const pse_info = {
+    /*const pse_info = {
       bank: "1022",
       invoice: clientePlanData.id?.toString(),
       description: "pay test",
@@ -507,7 +339,7 @@ export class ClientePlanController {
       email: "no-responder@payco.co",
       country: "CO",
       cell_phone: "3010000001",
-      ip: "190.000.000.000", /*This is the client's IP, it is required */
+      ip: "190.000.000.000", /*This is the client's IP, it is required
       url_response: "https://ejemplo.com/respuesta.html",
       url_confirmation: "https://ejemplo.com/confirmacion",
       metodoconfirmacion: "GET",
@@ -524,7 +356,7 @@ export class ClientePlanController {
     if (!bankTransaction || !bankTransaction.success) {
       console.error("Error en la transacción bancaria:", bankTransaction);
       throw new HttpErrors.InternalServerError("Error en la transacción bancaria");
-    }
+    }*/
   }
 }
 
